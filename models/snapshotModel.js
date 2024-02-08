2a59d9c17beeb3e4d9432460ca8c4d9c87a7b63b
Pelepo <andretta.filippo.99@gmail.com>
2024-02-08 11:20:03 +0100

    date unique

--- models/snapshotModel.js
+++ models/snapshotModel.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const SnapshotSchema = new mongoose.Schema({
     date: {
-        type: Date,
-        default: Date.now,
+        type: String,
         unique: true,
     },
 
