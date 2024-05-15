28db6185030fb415b672413be393a82e641e1ff4
Pelepo <andretta.filippo.99@gmail.com>
2024-05-15 10:40:11 +0200

    artist_contract_id

--- models/userModel.js
+++ models/userModel.js
@@ -28,6 +28,10 @@ const userSchema = new mongoose.Schema({
         type: String,
     },
 
+    artist_contract_id: {
+        type: String,
+    },
+
     artist_name: {
         type: String,
     },
