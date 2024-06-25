a8d1c1376a9efe5df655e0eebd1c9ee05012fab4
gianmariacarnazzi <gianmaria.carnazzi@gmail.com>
2024-06-25 15:22:09 +0200

    getContractTransactions token_id & blockrange fix

--- controllers/tokenTransactionControllers.js
+++ controllers/tokenTransactionControllers.js
@@ -5,6 +5,7 @@ const AppError = require("../Utils/appError");
 const { getContractEvents, createThirdwebClient, getContract, } = require("thirdweb");
 const { tokensClaimedEvent } = require("thirdweb/extensions/erc1155");
 const { polygonAmoy, polygon } = require("thirdweb/chains");
+const { Alchemy, Network } = require("alchemy-sdk");
 
 exports.getAllTransactions = catchAsync(async (req, res, next) => {
     const features = new APIFeatures(TokenTransaction.find(), req.query)
@@ -25,6 +26,16 @@ exports.getAllTransactions = catchAsync(async (req, res, next) => {
 
 exports.getContractTransactions = catchAsync(async (req, res, next) => {
     try {
+        const alchemyNetwork = process.env.ALCHEMY_NETWORK == "MATIC_MAINNET" ? Network.MATIC_MAINNET : Network.MATIC_AMOY;
+        const apiKey = process.env.ALCHEMY_NETWORK == "MATIC_MAINNET" ? process.env.ALCHEMY_API_KEY : process.env.ALCHEMY_API_KEY_TEST;
+        const config = {
+            apiKey: apiKey,
+            network: alchemyNetwork,
+        };
+        const alchemy = new Alchemy(config);
+
+        const blockRange = await alchemy.core.getBlockNumber(); // Ensure this is awaited
+
         const token_id = req.params.token_id;
         const token_address = req.params.token_address;
 
@@ -40,17 +51,16 @@ exports.getContractTransactions = catchAsync(async (req, res, next) => {
         const events = await getContractEvents({
             contract: contract,
             events: [
-                tokensClaimedEvent({
-                    tokenId: token_id,
-                })
+                tokensClaimedEvent()
             ],
-            blockRange: 1000000
+            blockRange: blockRange
         });
 
-        console.log(events);
+        // Filter events based on tokenId
+        const filteredEvents = events.filter(event => event.args.tokenId.toString() === token_id);
 
         // Extract the required fields from each event and convert BigInt values to strings
-        const formattedEvents = events.map(event => ({
+        const formattedEvents = filteredEvents.map(event => ({
             transactionHash: event.transactionHash,
             quantityClaimed: event.args.quantityClaimed.toString(),
             receiver: event.args.receiver
@@ -68,7 +78,6 @@ exports.getContractTransactions = catchAsync(async (req, res, next) => {
         console.log(error);
     }
 });
-
 
 
 exports.createNFTTransactionRecords = catchAsync(async (req, res, next) => {
