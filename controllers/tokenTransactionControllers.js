7e08cc832c37da78547fbcb3aefdc961e3c18c3d
gianmariacarnazzi <gianmaria.carnazzi@gmail.com>
2024-06-25 12:34:19 +0200

    getContractTransactions API call

--- controllers/tokenTransactionControllers.js
+++ controllers/tokenTransactionControllers.js
@@ -2,6 +2,9 @@ const TokenTransaction = require("../models/tokenTransactionModel");
 const APIFeatures = require("../Utils/apiFeatures");
 const catchAsync = require("../Utils/catchAsync");
 const AppError = require("../Utils/appError");
+const { getContractEvents, createThirdwebClient, getContract, } = require("thirdweb");
+const { tokensClaimedEvent } = require("thirdweb/extensions/erc1155");
+const { polygonAmoy, polygon } = require("thirdweb/chains");
 
 exports.getAllTransactions = catchAsync(async (req, res, next) => {
     const features = new APIFeatures(TokenTransaction.find(), req.query)
@@ -19,6 +22,54 @@ exports.getAllTransactions = catchAsync(async (req, res, next) => {
         },
     });
 });
+
+exports.getContractTransactions = catchAsync(async (req, res, next) => {
+    try {
+        const token_id = req.params.token_id;
+        const token_address = req.params.token_address;
+
+        const client = createThirdwebClient({
+            clientId: process.env.THIRDWEB_PROJECT_ID,
+        });
+
+        const chain = process.env.ACTIVE_CHAIN == "polygon" ? polygon : polygonAmoy;
+
+        const contract = getContract({ client, chain, address: token_address });
+        console.log(contract);
+
+        const events = await getContractEvents({
+            contract: contract,
+            events: [
+                tokensClaimedEvent({
+                    tokenId: token_id,
+                })
+            ],
+            blockRange: 1000000
+        });
+
+        console.log(events);
+
+        // Extract the required fields from each event and convert BigInt values to strings
+        const formattedEvents = events.map(event => ({
+            transactionHash: event.transactionHash,
+            quantityClaimed: event.args.quantityClaimed.toString(),
+            receiver: event.args.receiver
+        }));
+
+        // Send response
+        res.status(200).json({
+            status: "success",
+            results: formattedEvents.length,
+            data: {
+                events: formattedEvents,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
 
 exports.createNFTTransactionRecords = catchAsync(async (req, res, next) => {
     const newNFTtransaction = await TokenTransaction.create(req.body);
