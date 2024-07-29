a066d630b90980d19e86899a6068581ab2f30629
gianmariacarnazzi <gianmaria.carnazzi@gmail.com>
2024-07-29 18:54:50 +0200

    artist from notification fix

--- controllers/artistFormController.js
+++ controllers/artistFormController.js
@@ -45,27 +45,35 @@ exports.createEmail = catchAsync(async (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Your token has been sold </title>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400&display=swap" rel="stylesheet" >
+        <title>LIR Artist Application</title>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    
         <style>
-    .fab:hover {
-            color: rgb(214, 11, 82); /* Change the color on hover */
-        }
-        body, h1, p, a {
-            font-family: 'Space Grotesk', sans-serif;
-        }
+            .fab:hover {
+                color: rgb(214, 11, 82); /* Change the color on hover */
+            }
+            body, h1, p, a {
+                font-family: 'Space Grotesk', sans-serif;
+            }
         </style>
     </head>
-    <div style="color:white; background-color:rgb(17,17,17); font-family:sans-serif; padding: 50px 10%; overflow: auto">
-            <div style="margin: 50px 0">
-    <h1 style="color:rgb(214, 11, 82); text-align:center; text-transform:uppercase; margin: 0;">Notification</h1>
-    <div style="text-align:center; font-size: 18px; font-family: 'Space Grotesk', sans-serif">Congrats, you completed the form! LIR Team will reach you back soon.</div> 
-    </div>
-    
-          </div>
-          </html>`
+    <body style="color:white; background-color:rgb(17,17,17); font-family:sans-serif; padding: 50px 10%; overflow: auto">
+        <div style="margin: 50px 0">
+            <h1 style="color:rgb(214, 11, 82); text-align:center; text-transform:uppercase; margin: 0;">Notification</h1>
+            <div style="text-align:center; font-size: 18px; font-family: 'Space Grotesk', sans-serif">
+                Hey,<br><br>
+                Thank you for applying to LIR. Nice to meet you; I'm Filippo.<br><br>
+                I would love to have a quick call with you (15 minutes) to explain everything about the project. Do you prefer another channel rather than email for communication?<br><br>
+                Thank you for your interest.<br><br>
+                Best regards,<br><br>
+                Filippo Andretta<br>
+                CEO<br><br>
+                +39 334 3973277<br>
+                <a href="https://lirmusic.com" style="color: rgb(214, 11, 82); text-decoration: none;">lirmusic.com</a> | <a href="mailto:info@lirmusic.com" style="color: rgb(214, 11, 82); text-decoration: none;">info@lirmusic.com</a>
+            </div>
+        </div>
+    </body>
+    </html>`
     );
 });
 
