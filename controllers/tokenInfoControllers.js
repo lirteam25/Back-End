f68f5bb14fca9b7cab9dbe3cdc4f3bb5407cad02
Pelepo <andretta.filippo.99@gmail.com>
2024-08-11 12:44:51 +0200

    Pushing

--- controllers/tokenInfoControllers.js
+++ controllers/tokenInfoControllers.js
@@ -280,7 +280,7 @@ exports.addComment = catchAsync(async (req, res, next) => {
     // Find the user making the comment
     const user = await User.findOne(req.user);
     if (!user) {
-        return next(new AppError("No User found with that email", 404));
+        return next(new AppError("No User found with that token", 404));
     }
 
     // Create the new comment object
@@ -305,6 +305,65 @@ exports.addComment = catchAsync(async (req, res, next) => {
     if (!nft) {
         return next(new AppError("No NFT found with that ID", 404));
     }
+
+    const artist = await User.findOne({ uid: nft.author_address[0] });
+    if (!artist) {
+        return next(new AppError("Artist not found", 404));
+    }
+
+    await sendEmail(artist.artist_email, "LIR MUSIC - A new comment on your track",
+        `<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your track has been commented on</title>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <style>
+        .fab:hover {
+            color: rgb(214, 11, 82);
+        }
+        body, h1, p, a {
+            font-family: 'Space Grotesk', sans-serif;
+        }
+    </style>
+</head>
+<body style="color:white; background-color:rgb(17,17,17); font-family:sans-serif; padding: 50px 10%; overflow: auto">
+    <div style="margin: 50px 0">
+        <h1 style="color:rgb(214, 11, 82); text-align:center; text-transform:uppercase; margin: 0;">New Comment on Your Track</h1>
+        <div style="text-align:center; font-size: 18px; font-family: 'Space Grotesk', sans-serif">someone has commented on your track</div> 
+    </div>
+    <div style="background-color:rgb(27,27,27); padding: 10px 30px; border: 1px solid rgb(48, 48, 48); margin: 40px 0; font-size: 18px;">
+        <p style="margin: 20px 0;">Dear ${artist.artist_name},</p>
+        <p style="margin: 20px 0">We are pleased to inform you that your track has received a new comment!</p>
+        <p style="margin: 20px 0"><strong>Comment:</strong></p>
+        <p style="margin: 20px 0; padding: 10px; background-color: rgb(37,37,37); border-left: 4px solid rgb(214, 11, 82);">${newComment.comment}</p>
+        <p style="margin: 20px 0">Commented by: ${newComment.user_display_name}</p>
+        <p style="margin: 20px 0">We encourage you to engage with your fans by replying to their comments and sharing this exciting update with your followers. Your interaction can help build a stronger connection with your audience.</p>
+        <p style="margin: 20px 0">If you have any questions or need assistance, please don't hesitate to reach out to our dedicated support team at <a href="mailto:info@lirmusic.com" style="color:rgb(214, 11, 82); text-decoration: none">info@lirmusic.com</a>.</p>
+        <p style="margin: 20px 0">Best regards,</p>
+        <p style="margin: 20px 0">The LIR Music Team</p>
+    </div>
+    <div style="display: grid; grid-template-columns: 0.2fr 0.1fr 1fr 1fr ; align-items: top; margin-bottom: 40px">
+        <svg id="Livello_1" data-name="Livello 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 850.39 340.16" style="width: 80px; display: block;">
+            <defs><style>.cls-1{fill:#fff;}</style></defs>
+            <path class="cls-1" d="M237.27-74.54V95.54H208.92V-74.54ZM38.85,67.19V-74.54H10.5V95.54H180.58V67.19Zm315-56.69,49.09,85H435.7l-49.1-85Zm-88.25-85v28.35H407.35V10.5H435.7v-85Zm-422,0,49.1,85-49.1,85h32.73l49.1-85-49.1-85Zm-258.33-85v85h28.35v-56.69h283.46v56.69h28.35v-85Zm49.1,85-49.1,85,49.1,85h32.73L-382,10.5l49.09-85Zm262.71,226.77H-386.35V95.54H-414.7v85H-74.54v-85h-28.35Z" transform="translate(414.7 159.58)"/>
+        </svg>
+        <div style="font-size: 16px; font-family: 'Space Grotesk', sans-serif; grid-column: 3">
+            ©2023 LIR, all rights reserved <br/>
+            <a href="https://www.lirmusic.com" style="color: white; text-decoration: none">lirmusic.com</a>
+        </div>
+        <div style="display: flex; gap: 20px; justify-content: flex-end">
+            <a href="https://www.instagram.com/lirmusicofficial" style="color: white"> <i class="fab fa-instagram" style="font-size:23px"></i> </a>
+            <a href="https://discord.com/" style="color: white"> <i class="fab fa-discord" style="font-size:23px"></i> </a>
+            <a href="https://www.youtube.com/@lirmusicofficial" style="color: white"> <i class="fab fa-youtube" style="font-size:23px"></i> </a>
+        </div>
+    </div>
+</body>
+</html>
+`)
+
 
     // Send the updated NFT as the response
     res.status(200).json({
